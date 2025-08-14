54cd569bdeb000fdbf712bb6ac95ff9bc1d706b5
priya <sivapragasampriya01@gmail.com>
2025-08-14 21:49:07 +0530

    Refactor layout and index components; update Tailwind configuration and add new color palette

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
 import { Text, View } from "react-native";
+// import './global.css' // <--- REMOVE THIS LINE
+ 
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-accent font-bold">
+        Welcome!
+      </Text>
     </View>
   );
 }
