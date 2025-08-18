442d1ab14b0f778d95484676ce7a88afff677d69
priya <sivapragasampriya01@gmail.com>
2025-08-19 00:39:31 +0530

    Enhance tab navigation with icons and styling in layout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,47 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import { Tabs } from "expo-router";
-
+import {images} from "@/constants/images"
 export default function _Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          headerShown: false,
-        }}
-      />
-        <Tabs.Screen
-            name="search"
-            options={{
-            title: "Search",
-            headerShown: false,
-            }}/>
-        <Tabs.Screen
-            name="saved"
-            options={{
-            title: "Saved",
-            headerShown: false,
-            }}/>
-        <Tabs.Screen
-            name="profile"
-            options={{
-            title: "Profile",
-            headerShown: false,
-            }}/>
-    </Tabs>
-  );
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: 'blue',
+      tabBarInactiveTintColor: 'gray',
+      tabBarStyle: { backgroundColor: 'white' },
+      headerShown: false,
+    }}
+  >
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: "Home",
+        tabBarIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />,
+      }}
+    />
+    <Tabs.Screen
+      name="search"
+      options={{
+        title: "Search",
+        tabBarIcon: ({ color }) => <FontAwesome name="search" size={24} color={color} />,
+      }}
+    />
+    <Tabs.Screen
+      name="saved"
+      options={{
+        title: "Saved",
+        tabBarIcon: ({ color }) => <FontAwesome name="bookmark" size={24} color={color} />,
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Profile",
+        tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />,
+      }}
+    />
+  </Tabs>
+);
 }
